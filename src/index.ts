db7c54674eebc503a381e8d2d3b960b1ed103d19
martinanoailles <104039739+martinanoailles@users.noreply.github.com>
2022-06-06 21:39:37 -0300

    Otra variable posible

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,14 @@ let totalDescuento: number = precioProducto * descuento10Porciento;
 let precioFinalConDescuento: number = precioProducto - totalDescuento;
 
 //Consola
-console.log(precioFinalConDescuento);
+console.log("El precio final es:" precioFinalConDescuento);
+
+//Otra manera con una variable menos:
+let precioProducto1: number = 450.5;
+let descuento10Porciento1: number = 0.1;
+let precioFinalConDescuento1: number = precioProducto1 - (precioProducto1 * descuento10Porciento1);
+console.log ("El mismo precio final es:" precioFinalConDescuento1)
+
+/* Acá lo único que no comprendo es por qué me marca los precioFinalConDescuento
+en color rojo. Si le saco el texto de adelante me deja e aparecer en rojo. 
+Pero no entiendo por qué asi como esta me lo marca igual. */
